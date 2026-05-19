a9c232e181ef45abcfcebc9edab129a510f17781
ohdoyoel <ohdoyoel@gmail.com>
2026-05-19 23:06:01 +0900

    feat(dataset): zenodo aliases

--- src/main/java/astramut/dataset/BugFix.java
+++ src/main/java/astramut/dataset/BugFix.java
@@ -1,13 +1,14 @@
 package astramut.dataset;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record BugFix(
-    @JsonProperty("bugType")              String bugType,
-    @JsonProperty("sourceBeforeFix")      String sourceBeforeFix,
-    @JsonProperty("sourceAfterFix")       String sourceAfterFix
+    @JsonProperty("bugType")                                       String bugType,
+    @JsonProperty("sourceBeforeFix") @JsonAlias({"before"})        String sourceBeforeFix,
+    @JsonProperty("sourceAfterFix")  @JsonAlias({"after"})         String sourceAfterFix
 ) {
     public boolean hasSourceCode() {
         return sourceBeforeFix != null && !sourceBeforeFix.isBlank()
