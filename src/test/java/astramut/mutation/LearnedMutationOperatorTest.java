33d9d5815ce7e575bc7757fe33c5a3c93378af2c
Junseo Jang <lily050402@gmail.com>
2026-05-21 08:30:38 +0000

    test(mutation): update learned mutation operator test

--- src/test/java/astramut/mutation/LearnedMutationOperatorTest.java
+++ src/test/java/astramut/mutation/LearnedMutationOperatorTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class LearnedMutationOperatorImprovementTest {
+class LearnedMutationOperatorTest {
     @Test
     void mutatesScopedMethodCallReceiverAndArguments() {
         EditPattern fixPattern = new EditPattern(
